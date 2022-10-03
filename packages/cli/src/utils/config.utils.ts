bdd45ea0df816d97c52b4800f006b86104c74e2f
Kevin Orel Edry <kevin@youleap.com>
2022-10-03 09:41:23 +0300

    changes sdk and api to work with stg env

--- packages/cli/src/utils/config.utils.ts
+++ packages/cli/src/utils/config.utils.ts
@@ -1,6 +1,6 @@
 export const configUtility = {
   get youleapApiBaseUrl(): string {
-    return process.env['YOULEAP_API_BASE_URL'] ?? 'https://gateway.youleap-local.io/';
+    return process.env['YOULEAP_API_BASE_URL'] ?? 'https://gateway.youleap.io/';
   },
   get auth0ApiBaseUrl(): string {
     return process.env['AUTH0_API_BASE_URL'] ?? 'https://youleap.eu.auth0.com/';
